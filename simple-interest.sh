122b3f27623afc7beb0d3153eef9ae0e60de70de
Harry-Hoang <108538215+Harry-Hoang@users.noreply.github.com>
2024-01-21 09:48:16 +0700

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -2,10 +2,10 @@
 # This script calculates simple interest given principal,
 # annual rate of interest and time period in years.
 # Do not use this in production. Sample purpose only.
-# ID: <your student id>
-# Author: <your name>
-# Additional Authors: MR. NAM
-# <your GitHub username>
+# ID: <21002585>
+# Author: <Hai>
+# Additional Authors: MR. Hai
+# <Harry-Hoang>
 # Input:
 # p, principal amount
 # t, time period in years
